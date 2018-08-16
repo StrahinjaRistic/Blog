fca51a42d7694e0d39bf9881ecd4d65c94e2b1a3
BorisVukelic <slobodan.vukelic@gmail.com>
2018-08-16 15:52:09 +0200

    aaa

--- src/Blog.vue
+++ src/Blog.vue
@@ -1,11 +1,24 @@
 <template lang="html">
   <div>
-    <h1>blogovi</h1>
+    <ul>
+      <li v-for="blog in blogList">{{blog.blo_naslov}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
+  data:function(){
+    return{
+      blogList:[]
+    }
+  },
+  mounted(){
+      axios.get("http://741a121.mars-e1.mars-hosting.com/api/blog")
+      .then(response => {
+      this.blogList = response.data.rezultat;
+      });
+    }
 }
 </script>
 
--- src/IzbrisiBlog.vue
+++ src/IzbrisiBlog.vue
@@ -1,11 +1,35 @@
 <template lang="html">
   <div>
-    <h1>brisanje</h1>
+    <ul>
+      <li v-for="blog in blogList">{{blog.blo_naslov}} <button @click="obrisiBlog(blog.blo_id)">X</button> </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
+  data:function(){
+    return{
+      blogList:[]
+    }
+  },
+  mounted(){
+    this.poziv();
+  },
+  methods:{
+    obrisiBlog(index){
+      axios.delete("http://741a121.mars-e1.mars-hosting.com/api/blog",{ params:{id:index} })
+      .then(()=>{
+        this.poziv();
+      })
+    },
+    poziv(){
+      axios.get("http://741a121.mars-e1.mars-hosting.com/api/blog")
+      .then(response => {
+        this.blogList = response.data.rezultat;
+      });
+    }
+  }
 }
 </script>
 
