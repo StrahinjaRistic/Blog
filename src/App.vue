f6465f4986318e1ede26eff49fbeb525d659c695
BorisVukelic <slobodan.vukelic@gmail.com>
2018-08-16 16:28:29 +0200

    dodao navigejsn bar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,30 @@
 <template>
   <div id="app">
-    <button @click="komp = 'blog'">Prikazi Blogove</button>
-    <button @click="komp = 'dodajBlog'">Dodaj Blog</button>
-    <button @click="komp = 'izbrisiBlog'">Izbrisi Blog</button>
+    <nav class="nav-wraper">
+      <div class="container">
+        <a class="brand-logo">Blog</a>
+        <a class="sidenav-trigger" data-target="mobile-links">
+          <i class="material-icons">menu</i>
+        </a>
+      <ul class="right hide-on-med-and-down">
+        <li><a @click="komp = 'blog'">Prikazi Blogove</a></li>
+        <li><a @click="komp = 'dodajBlog'">Dodaj Blog</a></li>
+        <li><a @click="komp = 'izbrisiBlog'">Izbrisi Blog</a></li>
+     </ul>
+   </div>
+   </nav>
+
+   <ul class="sidenav" id="mobile-links">
+     <li><a @click="komp = 'blog'">Prikazi Blogove</a></li>
+     <li><a @click="komp = 'dodajBlog'">Dodaj Blog</a></li>
+     <li><a @click="komp = 'izbrisiBlog'">Izbrisi Blog</a></li>
+   </ul>
+
     <keep-alive>
+      <div class="container">
       <component :is="komp">
       </component>
+    </div>
     </keep-alive>
   </div>
 </template>
