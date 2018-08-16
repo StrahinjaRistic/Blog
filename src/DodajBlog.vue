3c38bb9d1ee254714db956fd7e94dd83fbc2113b
StrahinjaRistic <st_raja@hotmail.com>
2018-08-16 15:51:12 +0200

    a

--- src/DodajBlog.vue
+++ src/DodajBlog.vue
@@ -1,11 +1,28 @@
 <template lang="html">
   <div>
-    <h1>dodavanje</h1>
+    <label>Blogovi</label><br>
+      <textarea rows="1" cols="20" v-model="naslov"></textarea><br>
+      <textarea rows="5" cols="50" v-model="text"></textarea>
+      <button @click.prevent="dodajBlog">Dodaj Blog</button>
   </div>
 </template>
 
 <script>
 export default {
+  data: function(){
+    return{
+      naslov:'',
+      text:''
+    }
+  },
+  methods:{
+    dodajBlog(){
+    axios.post("http://741a121.mars-e1.mars-hosting.com/api/blog",{naslov: this.naslov, text: this.text})
+    alert("Blog je uspesno dodat!!!");
+    }
+  }
+
+
 }
 </script>
 
